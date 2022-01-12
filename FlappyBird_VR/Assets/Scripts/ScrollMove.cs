6e7c69c16f91416cd9e4006a3e69e8c1f4882a19
Thymho <rhcowls@naver.com>
2022-01-12 22:32:13 +0900

    Flappy_Bird
    Flappy_Bird 게임 프로젝트

---
+++ FlappyBird_VR/Assets/Scripts/ScrollMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMove : MonoBehaviour
+{
+    public float scrollSpeed;
+    float target0ffset;
+
+    void Update()
+    {
+        target0ffset += Time.deltaTime * scrollSpeed;
+        GetComponent<Renderer>().material.mainTextureOffset =
+            new Vector2(target0ffset, -0.01f);
+    }
+}
